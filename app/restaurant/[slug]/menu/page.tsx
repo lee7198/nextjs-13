c9d666906d538130289685704f426f9a0dc06dd0
lee7198 <lee7198@naver.com>
2023-06-24 00:19:31 +0900

    s5 fetching data in server components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,12 +1,36 @@
-import Header from "../../../components/Header";
 import RestaurantNavBar from "../components/RestaurantNavBar";
 import Menu from "../components/Menu";
+import { PrismaClient } from "@prisma/client";
 
-export default function RestaurantMenu({ slug }: { slug: string }) {
+const prisma = new PrismaClient();
+
+const fetchRestaurantItems = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      items: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error("Restaurant not found");
+  }
+  return restaurant.items;
+};
+
+export default async function RestaurantMenu({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const menu = await fetchRestaurantItems(params.slug);
+
   return (
     <div className="bg-white w-[100%] rounded p-3 shadow">
-      <RestaurantNavBar slug={slug} />
-      <Menu />
+      <RestaurantNavBar slug={params.slug} />
+      <Menu menu={menu} />
     </div>
   );
 }
