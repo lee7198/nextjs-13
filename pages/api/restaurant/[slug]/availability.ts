600ef5c8fee1032651cf2777ec5736379ff4f189
lee7198 <lee7198@naver.com>
2023-07-07 23:11:53 +0900

    l.100

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -16,14 +16,14 @@ export default async function handler(
   };
 
   if (!day || !time || !partySize)
-    return res.status(400).json({ errorMessage: "Missing query params" });
+    return res.status(400).json({ errorMessage: "Invalid data provided" });
 
   const searchTimes = times.find((t) => {
     return t.time === time;
   })?.searchTimes;
 
   if (!searchTimes)
-    return res.status(400).json({ errorMessage: "Missing query params" });
+    return res.status(400).json({ errorMessage: "Invalid data provided" });
 
   const bookings = await prisma.booking.findMany({
     where: {
@@ -39,7 +39,52 @@ export default async function handler(
     },
   });
 
-  return res.status(200).json({ searchTimes, bookings });
+  const bookingTablesObject: { [key: string]: { [key: string]: true } } = {};
+
+  bookings.forEach((booking) => {
+    bookingTablesObject[booking.booking_time.toISOString()] =
+      booking.tables.reduce((obj, table) => {
+        return { ...obj, [table.table_id]: true };
+      }, {});
+  });
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      tables: true,
+    },
+  });
+  if (!restaurant)
+    return res.status(400).json({ errorMessage: "Invalid data provided" });
+
+  const tables = restaurant.tables;
+
+  const searchTimesWithTables = searchTimes.map((searchTime) => {
+    return {
+      date: new Date(`${day}T${searchTime}`),
+      time: searchTime,
+      tables,
+    };
+  });
+
+  searchTimesWithTables.forEach((t) => {
+    t.tables = t.tables.filter((table) => {
+      if (bookingTablesObject[t.date.toISOString()])
+        if (bookingTablesObject[t.date.toISOString()][table.id]) return false;
+
+      return true;
+    });
+  });
+
+  return res.status(200).json({
+    searchTimes,
+    bookings,
+    bookingTablesObject,
+    tables,
+    searchTimesWithTables,
+  });
 }
 
 //localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-01-01&time=20:00:00.000Z&partySize=4
