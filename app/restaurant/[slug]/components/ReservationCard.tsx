92bda2293514b6fbdea7213aa96d5d19280ef992
lee7198 <lee7198@naver.com>
2023-07-08 23:42:33 +0900

    section 10

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,20 +1,33 @@
 "use client";
 import { useState } from "react";
-import { partySize, times } from "../../../../data";
-
+import { partySize as PartySizes, times } from "../../../../data";
 import DatePicker from "react-datepicker";
-import { time } from "console";
+import useAvailabilities from "../../../../hooks/useAvailabilities";
+import { CircularProgress } from "@mui/material";
+import Link from "next/link";
+import {
+  Time,
+  convertToDisplayTime,
+} from "../../../../utils/convertToDisplayTime";
 
 export default function ReservationCard({
   openTime,
   closeTime,
+  slug,
 }: {
   openTime: string;
   closeTime: string;
+  slug: string;
 }) {
+  const { data, loading, error, fetchAvailabilities } = useAvailabilities();
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [time, setTime] = useState(openTime);
+  const [partySize, setPartySize] = useState("6");
+  const [day, setDay] = useState(new Date().toISOString().split("T")[0]);
+
   const handleChange = (date: Date | null) => {
     if (date) {
+      setDay(date.toISOString().split("T")[0]);
       return setSelectedDate(date);
     }
     return setSelectedDate(null);
@@ -34,6 +47,15 @@ export default function ReservationCard({
     return timesWithinWindow;
   };
 
+  const handleClick = () => {
+    fetchAvailabilities({
+      slug,
+      day,
+      time,
+      partySize,
+    });
+  };
+
   return (
     <div className="fixed w-[15%] bg-white rounded p-3 shadow">
       <div className="text-center border-b pb-2 font-bold">
@@ -41,8 +63,14 @@ export default function ReservationCard({
       </div>
       <div className="my-3 flex flex-col">
         <label htmlFor="">Party size</label>
-        <select name="" className="py-3 border-b font-light" id="">
-          {partySize.map((item) => (
+        <select
+          name=""
+          className="py-3 border-b font-light"
+          id=""
+          value={partySize}
+          onChange={(e) => setPartySize(e.target.value)}
+        >
+          {PartySizes.map((item) => (
             <option value={item.value} key={item.label}>
               {item.label}
             </option>
@@ -62,7 +90,13 @@ export default function ReservationCard({
         </div>
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Time</label>
-          <select name="" id="" className="py-3 border-b font-light">
+          <select
+            name=""
+            id=""
+            className="py-3 border-b font-light"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          >
             {filterTimeByRestaurantOpenWindow().map((item) => (
               <option key={item.time} value={item.time}>
                 {item.displayTime}
@@ -72,10 +106,39 @@ export default function ReservationCard({
         </div>
       </div>
       <div className="mt-5">
-        <button className="bg-red-600 rounded w-full px-4 text-white font-bold h-16">
-          Find a Time
+        <button
+          className="bg-red-600 rounded w-full px-4 text-white font-bold h-16"
+          onClick={handleClick}
+          disabled={loading}
+        >
+          {loading ? <CircularProgress color="inherit" /> : "Find a Time"}
         </button>
       </div>
+      {data && data.length ? (
+        <div className="mt-4">
+          <p className="text-reg">Select a Time</p>
+          <div className="flex flex-wrap mt-2">
+            {data.map((t) => {
+              return t.available ? (
+                <Link
+                  key={t.time}
+                  href={`/reserve/${slug}?data=${day}T${t.time}&partySize=${partySize}`}
+                  className="bg-red-600 cursor-pointer p-2 w-24 text-center mb-3 rounded mr-3"
+                >
+                  <p className="text-sm font-bold">
+                    {convertToDisplayTime(t.time as Time)}
+                  </p>
+                </Link>
+              ) : (
+                <p
+                  key={t.time}
+                  className="bg-gray-300 p-2 w-24 mb-3 rounded mr-3"
+                ></p>
+              );
+            })}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -54,6 +54,8 @@ export default async function handler(
     },
     select: {
       tables: true,
+      open_time: true,
+      close_time: true,
     },
   });
   if (!restaurant)
@@ -78,13 +80,28 @@ export default async function handler(
     });
   });
 
-  return res.status(200).json({
-    searchTimes,
-    bookings,
-    bookingTablesObject,
-    tables,
-    searchTimesWithTables,
-  });
+  const availableities = searchTimesWithTables
+    .map((t) => {
+      const sumSeats = t.tables.reduce((sum, table) => {
+        return sum + table.seats;
+      }, 0);
+      return {
+        time: t.time,
+        available: sumSeats >= parseInt(partySize),
+      };
+    })
+    .filter((availability) => {
+      const timeIsAcfterOpeningHour =
+        new Date(`${day}T${availability.time}`) >=
+        new Date(`${day}T${restaurant.open_time}`);
+      const timeIsBeforeClosingHour =
+        new Date(`${day}T${restaurant.close_time}`) >=
+        new Date(`${day}T${availability.time}`);
+
+      return timeIsAcfterOpeningHour && timeIsBeforeClosingHour;
+    });
+
+  return res.status(200).json(availableities);
 }
 
 //localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-01-01&time=20:00:00.000Z&partySize=4
